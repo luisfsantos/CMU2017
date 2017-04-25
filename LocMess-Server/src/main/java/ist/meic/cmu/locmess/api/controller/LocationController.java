606f8415e1f2414b58d21fda5a1c6e5fd3fb9d4c
pauloanjos <paulo.15.gk@gmail.com>
2017-04-25 20:08:24 +0100

    add list Locations

--- LocMess-Server/src/main/java/ist/meic/cmu/locmess/api/controller/LocationController.java
+++ LocMess-Server/src/main/java/ist/meic/cmu/locmess/api/controller/LocationController.java
@@ -1,6 +1,7 @@
 package ist.meic.cmu.locmess.api.controller;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
@@ -8,6 +9,7 @@ import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
 import ist.meic.cmu.locmess.api.json.Error;
+import ist.meic.cmu.locmess.api.json.JsonListAPI;
 import ist.meic.cmu.locmess.api.json.JsonObjectAPI;
 import ist.meic.cmu.locmess.api.json.wrappers.LocationWrapper;
 import ist.meic.cmu.locmess.database.Settings;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.SQLException;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -57,6 +60,32 @@ public class LocationController {
         data.addProperty("status", "Location " + newLocation.getName() + " created");
         response.setData(data);
 
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+    @RequestMapping(value = "/list", method = RequestMethod.GET,produces ="application/json")
+    public ResponseEntity<JsonObjectAPI> list() {
+    	 JsonObjectAPI response = new JsonObjectAPI();;
+    	logger.info("request locations");
+    	List<Location> locations=null;
+    	try {
+            ConnectionSource connectionSource =
+                    new JdbcConnectionSource(Settings.DB_URI);
+
+            Dao<Location, String> locationDAO = DaoManager.createDao(connectionSource, Location.class);
+            TableUtils.createTableIfNotExists(connectionSource, Location.class);
+            locations= locationDAO.queryForAll();
+            connectionSource.close();
+
+        } catch (SQLException e) {
+            response.addError(new Error(2, "Locations could not be listed please try again later"));
+            e.printStackTrace();
+            return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+        }
+    	JsonObject data = new JsonObject();
+        data.addProperty("code", 0);
+        data.addProperty("status", "List locations with sucess");
+        data.addProperty("locations", gson.toJson(locations));
+        response.setData(data);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
